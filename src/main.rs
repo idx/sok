2986c12e05c24c2fde36db4815d5a887c7ad9297
idx <idxx@outlook.jp>
2022-11-10 01:02:59 +0900

    Add query argument

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   query: String,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    println!("QUERY: {}", args.query)
 }
